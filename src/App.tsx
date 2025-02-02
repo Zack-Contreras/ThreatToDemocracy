a4bf499f60b071889f2bd43b6c67d827fed72c00
Zackary Contreras <zackaryc007@gmail.com>
2025-02-01 20:31:11 -0800

    website v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import tddLogo from "./assets/tdd.png";
+import bsLogo from "./assets/Bluesky_Logo.svg.png";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="content-container">
+      <img className="logo" alt="Threat To Democracy Logo" src={tddLogo} />
+      <div className="content">
+        <h1>Join the movement</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Fascism and authoritarianism are on the rise, threatening the
+          foundation of our democratic society. Now more than ever, we must come
+          together to raise awareness and take action. If you want to be part of
+          building this platform for change,{" "}
+          <a target="_blank" href="https://discord.gg/cbwRyYBt">
+            join us on Discord
+          </a>
+          .
         </p>
+        <a
+          target="_blank"
+          href="https://bsky.app/profile/threat-2-democracy.bsky.social"
+        >
+          Follow us <img height={12} src={bsLogo} />
+        </a>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
